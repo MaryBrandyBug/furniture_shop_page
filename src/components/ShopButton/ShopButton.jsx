9f03dd91041d72a2ca61889a3512148f39c40fc9
Maria <17mashmallow@gmail.com>
2023-04-15 00:27:50 +0300

    modules added

--- src/components/ShopButton/ShopButton.jsx
+++ src/components/ShopButton/ShopButton.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './ShopButton.module.scss';
 
 // eslint-disable-next-line react/prop-types
 export default function ShopButton({ sectionStyle }) {
   return (
-    <button className={`shop-link__button bold-font-style ${sectionStyle}`} type="button">shop</button>
+    <button className={`${styles['shop-link__button']} bold-font-style ${sectionStyle || ''}`} type="button">shop</button>
   );
 }
