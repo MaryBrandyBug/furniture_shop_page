b238b929e6ec3bce9ba07cce376338d0040e098f
Maria <17mashmallow@gmail.com>
2023-04-15 20:46:34 +0300

    added one more photo block to galary

--- src/components/Galary/data.js
+++ src/components/Galary/data.js
@@ -1,2 +1,4 @@
 export const sofaBlockText = 'Cut / vert sablé';
 export const benchBlockText = 'Linéaire et minimaliste, les bancs Galta dessinés par SCMP Design Office';
+export const chairBlockText = 'tal / chêne noir';
+export const chairBlockLink = 'voir toutes les chaises';
--- src/components/PhotoBlock/PhotoBlock.jsx
+++ src/components/PhotoBlock/PhotoBlock.jsx
@@ -3,11 +3,18 @@ import styles from './PhotoBlock.module.scss';
 import { ShopButton } from '../ShopButton';
 
 export default function PhotoBlock({
-  photoPath1, alt1, photoPath2, alt2, text,
+  photoPath1, alt1, photoPath2, alt2, text, linkText,
 }) {
   return (
     <div className={styles.block}>
-      <img src={photoPath1} alt={alt1} />
+      {linkText
+        ? (
+          <div className={styles.block__photo}>
+            <img src={photoPath1} alt={alt1} />
+            <p className={`${styles.block__photo__link} regular-font-style block-links`}>{linkText}</p>
+          </div>
+        )
+        : <img src={photoPath1} alt={alt2} />}
       <div className={styles.smoller_block}>
         <img src={photoPath2} alt={alt2} />
         <div className={styles.smoller_block__links}>
