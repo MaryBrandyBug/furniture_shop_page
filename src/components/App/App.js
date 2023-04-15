3a84d53f14fe4f6e24c109700b03626c0a85365c
Maria <17mashmallow@gmail.com>
2023-04-16 02:12:35 +0300

    added subscription

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,15 @@
 import './App.scss';
 import { Header } from '../Header';
 import { Galary } from '../Galary';
+import { Subscription } from '../Subscription';
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
-      <div className="galary_container">
+      <div className="content_container">
         <Galary />
+        <Subscription />
       </div>
     </div>
   );
