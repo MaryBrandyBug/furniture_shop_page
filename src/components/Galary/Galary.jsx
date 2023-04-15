c3bfdef6bff61e2b5bd7acb72d5e3ada47640731
Maria <17mashmallow@gmail.com>
2023-04-15 03:21:29 +0300

    added sofas block

--- src/components/Galary/Galary.jsx
+++ src/components/Galary/Galary.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Galary.module.scss';
 import { ProductionBlock } from '../ProductionBlock';
+import SofaBlock from '../SofaBlock/SofaBlock';
 
 export default function Galary() {
   return (
     <div className={styles.galary}>
       <ProductionBlock />
+      <SofaBlock />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
       <Navbar />
       <div className={styles.header__content}>
         <h1 className={`${styles.header__content__text} bold-font-style`}>{textHeader}</h1>
-        <ShopButton sectionStyle={styles.header__content__btn} />
+        <ShopButton sectionStyle={styles.header__content__btn} fontParams="bold-font-style" />
       </div>
     </header>
   );
